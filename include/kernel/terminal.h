7b07a185569b6d60ce24d942a8529cab87f38849
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-04-26 10:02:11 +0200

    Added Terminal commands

--- include/kernel/terminal.h
+++ include/kernel/terminal.h
@@ -5,8 +5,13 @@
 
 #define EXIT 6384018879
 #define REBOOT 6952682532528
-#define CD 5862188
 #define PWD 193467056
+#define SHELL 210688646365
+#define LS 5862500
+#define CD 5862188
+#define TOUCH 210690100968
+#define MKDIR 210681637468
+#define RM 5862692
 
 void init_terminal();
 void execute_command(char *command);
--- kernel/terminal.c
+++ kernel/terminal.c
@@ -17,24 +17,82 @@ void init_terminal()
     print("LumaOS > ");
 }
 
+/* Commands */
+static void exit();
+static void reboot();
+static void pwd();
+static void shell();
+static void ls();
+static void cd();
+static void touch();
+static void mkdir();
+static void rm();
+
 void execute_command(char *command)
 {
     switch(hash(command))
     {
-        case EXIT:
-            print("Shutting down LumaOS!\n");
-            asm volatile("hlt");
-            break;
-        case REBOOT:
-            print("Trying to reboot LumaOS!\n");
-            // TODO
-            break;
-        default:
-            break; 
+        case EXIT: exit(); break;
+        case REBOOT: reboot(); break;
+        case PWD: pwd(); break;
+        case SHELL: shell(); break;
+        case LS: ls(); break;
+        case CD: cd(); break;
+        case TOUCH: touch(); break;
+        case MKDIR: mkdir(); break;
+        case RM: rm(); break;
+        default: break;
     }
     
     print("Unknown command: ");
     print(command);
     print_nl();
     init_terminal();
+}
+
+static void exit()
+{
+    print("Shutting down\n");
+    asm volatile("hlt");
+}
+
+static void reboot()
+{
+    print("Trying to reboot LumaOS!\n");
+}
+
+static void pwd()
+{
+    print("/");
+}
+
+static void shell()
+{
+    clear_screen();
+    init_terminal();
+}
+
+static void ls()
+{
+    print("NOT IMPLEMENTED");
+}
+
+static void cd()
+{
+    print("NOT IMPLEMENTED");
+}
+
+static void touch()
+{
+    print("NOT IMPLEMENTED");
+}
+
+static void mkdir()
+{
+    print("NOT IMPLEMENTED");
+}
+
+static void rm()
+{
+    print("NOT IMPLEMENTED");
 }
