1da48ef9ca73303a00b9c847054ec9886ad07258
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-06 13:30:54 +0200

    Delete include/kernel directory

--- include/kernel/kernel.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUMAOS_KERNEL_H_
-#define LUMAOS_KERNEL_H_
-
-#pragma once
-
-void init_kernel();
-
-#endif
--- include/kernel/usermode.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUAMOS_USERMODE_H_
-#define LUAMOS_USERMODE_H_
-
-#pragma once
-
-void switch_to_user_mode();
-
-#endif
