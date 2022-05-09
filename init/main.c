d6a139c609af7979c686965e998b88a9d5cbfb47
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-09 12:27:45 +0200

    Delete init directory

--- init/main.c
+++
@@ -1,8 +0,0 @@
-#include <lumaos/kernel/kernel.h>
-#include <system/multiboot.h>
-#include <stdint.h>
-
-void main(multiboot_header_t *multiboot_ptr, uint32_t initial_stack)
-{
-    kernel_main(initial_stack);
-}
