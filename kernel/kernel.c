2b4a645a5f9c997fbc80e925d54292fe46b8f71e
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-11 09:58:46 +0200

    Updating Kernel Main

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,35 @@
-#include "../include/kernel/kernel.h"
-#include "../include/cpu/isr.h"
-#include "../include/drivers/keyboard.h"
-#include "../include/drivers/display.h"
-#include "../include/kernel/terminal.h"
-#include "../include/kernel/usermode.h"
+#include <kernel/kernel.h>
+#include <kernel/gdt.h>
+#include <kernel/paging.h>
+#include <kernel/task.h>
+#include <kernel/syscall.h>
+#include <kernel/cpu/idt.h>
+#include <kernel/cpu/timer.h>
+
+#include <driver/keyboard.h>
+
+#include <fs/filesystem.h>
+#include <fs/initramdisk.h>
+
+#include <asm/system.h>
+#include <system/multiboot.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
 
 #if !defined(__i386__)
 #error "Must be compiled with i386-elf compiler!"
 #endif
+
+#define HANDLERS 256
+#define TIMER 50
+
+extern isr_t interrupt_handlers[];
+extern uint32_t placement_address;
+
+uint32_t initial_esp;
 
 char *str =
 " _                            ____   _____ \n"\
@@ -17,28 +39,40 @@ char *str =
 "| |___| |_| | | | | | | (_| | |__| |____) |\n"\
 "|______\\__,_|_| |_| |_|\\__,_|\\____/|_____/ \n";
 
-void init_kernel()
+int main(multiboot_t *mboot_ptr, uint32_t initial_stack)
 {
-    clear_screen();
-    
-    print("Installing interrupt service routines (ISRs).\n");
-    isr_install();
+    initial_esp = initial_stack;
 
-    print("Enabling external interrupts.\n");
-    asm volatile("sti");
+    gdt_init();
+    idt_init();
+    memset(&interrupt_handlers, 0, sizeof(isr_t) * HANDLERS);
 
-    print("Initializing keyboard (IRQ 1).\n");
+    cls();
+
+    STI();
+    timer_init(TIMER);
+
+    ASSERT(mboot_ptr->mods_count > 0);
+
+    uint32_t initrd_location = *((uint32_t*) mboot_ptr->mods_addr);
+    uint32_t initrd_end = *(uint32_t*)(mboot_ptr->mods_addr + 4);
+
+    placement_address = initrd_end;
+
+    init_paging();
+    init_tasking();
+
+    filesystem_root = init_initial_ram_disk();
+
+    init_syscalls();
+
+    switch_to_user_mode();
+
     init_keyboard();
-    
-    clear_screen();
-    
-    print(str);
 
-    //switch_to_user_mode();
-}
+    printf("%s", str);
 
-void main()
-{
-    init_kernel();
     init_terminal();
+
+    return 0;
 }
