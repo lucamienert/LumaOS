ca5efa850234d6f03aca80dcd80df93838e33a4d
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-13 13:36:52 +0200

    Updating GUI

--- include/gui/draw.h
+++ include/gui/draw.h
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #include <gui/wm.h>
 
+uint32_t *pixel_offset(fb_t fb, uint32_t x, uint32_t y);
+
 void draw_pixel(fb_t fb, int32_t x, int32_t y, uint32_t col);
 void draw_rect(fb_t fb, int32_t x, int32_t y, int32_t width, int32_t height, uint32_t col);
 void draw_line(fb_t fb, int32_t x, int32_t y, int32_t x2, int32_t y2, uint32_t col);
