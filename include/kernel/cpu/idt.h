f9b750cd1aab0a7804c4c86b5be832b807a14f0a
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-27 11:58:08 +0200

    Updating header files for IDT and ISR

--- include/kernel/cpu/idt.h
+++ include/kernel/cpu/idt.h
@@ -8,22 +8,24 @@
 #define KERNEL_CS 0x08
 #define IDT_ENTRIES 256
 
-typedef struct 
+typedef struct IDTEntry
 {
     uint8_t always0;
     uint8_t flags;
     uint16_t low_offset;
     uint16_t sel;
     uint16_t high_offset;
-} __attribute__((packed)) idt_gate_t;
+} __attribute__((packed)) idt_entry_t;
 
-typedef struct 
+typedef struct IDTPointer
 {
     uint16_t limit;
     uint32_t base;
-} __attribute__((packed)) idt_register_t;
+} __attribute__((packed)) idt_pointer_t;
 
-void set_idt_gate(int32_t n, uint32_t handler);
-void load_idt();
+extern void idt_flush(uint32_t);
+
+void idt_set_gate(int32_t n, uint32_t base, uint16_t sel, uint8_t flags);
+void idt_init();
 
 #endif
