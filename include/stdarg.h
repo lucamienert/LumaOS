6cab3faa566d29cfb4746f0855c4c70ed0b2162f
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-06-02 11:32:28 +0200

    Update stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,9 +1,10 @@
-#ifndef LUMALANG_STDARG_H_
-#define LUMALANG_STDARG_H_
+#ifndef LUMAOS_STDARG_H_
+#define LUMAOS_STDARG_H_
 
 #pragma once
 
 typedef char *va_list;
+
 #define va_start(ap,parmn) (void)((ap) = (char*)(&(parmn) + 1))
 #define va_end(ap) (void)((ap) = 0)
 #define va_arg(ap, type) \
